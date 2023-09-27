6b8857455ff0cdce4c6f1b74bba1c357238e9398
ZZY19980203 <89687351+ZZY19980203@users.noreply.github.com>
2023-09-27 12:52:21 +0800

    Update download.sh

--- teacher_model/download.sh
+++ teacher_model/download.sh
@@ -10,10 +10,3 @@ wget https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_diffu
 gdown https://drive.google.com/uc?id=1norNWWGYP3EZ_o05DmoW1ryKuKMmhlCX
 gdown https://drive.google.com/uc?id=1QEl-btGbzQz6IwkXiFGd49uQNTUtTHsk
 )
-
-# data
-(
-gdown https://drive.google.com/uc?id=1Q_dxuyI41AAmSv9ti3780BwaJQqwvwMv
-unzip data.zip
-rm data.zip
-)
