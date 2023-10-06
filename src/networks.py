39aacea21e1f2c0c6339086b871dc1b5a891f7ff
ZZY19980203 <89687351+ZZY19980203@users.noreply.github.com>
2023-10-06 18:15:29 +0800

    Update networks.py

--- src/networks.py
+++ src/networks.py
@@ -63,7 +63,7 @@ class InpaintGenerator(BaseNetwork):
         self.middle = nn.Sequential(*blocks)
 
         self.decoder = nn.Sequential(
-            # Att(256),
+            Att(256),
             nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=4, stride=2, padding=1),
             nn.InstanceNorm2d(128, track_running_stats=False),
             nn.ReLU(True),
@@ -72,7 +72,7 @@ class InpaintGenerator(BaseNetwork):
             nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=4, stride=2, padding=1),
             nn.InstanceNorm2d(64, track_running_stats=False),
             nn.ReLU(True),
-            Att(64),
+            # Att(64),
 
             nn.ReflectionPad2d(3),
             nn.Conv2d(in_channels=64, out_channels=3, kernel_size=7, padding=0),
